86bd3754ac75f7a323f1f8c74a93d849aa962c93
Niko Drossos <ndross427@west-mec.org>
2023-03-28 14:20:59 -0700

    Add REPO

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+/* ------------------ Make hacker text effect for home page ----------------- */
+
+const fancy = document.querySelectorAll('.fancy');
+const letters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+
+for (let element of fancy) {
+  element.addEventListener('mouseover', event => {
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+      event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+        if (index < iterations) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      }).join("");
+
+      if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+      iterations += 1 / 2;
+    }, 30);
+  });
+}
+
+
+
